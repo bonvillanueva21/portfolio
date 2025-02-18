6d0595286d3bbe8bcf61b07ee73ecdd072ace358
Angelo Villanueva <bonvillanueva21@gmail.com>
2025-02-18 14:36:29 +0800

    Remove mix-blend

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-scroll";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between md:mb-10 py-6 px-4 md:px-8 sticky top-0 z-50 bg-opacity-60 backdrop-blur-lg  mix-blend-difference">
+    <nav className="flex items-center justify-between md:mb-10 py-6 px-4 md:px-8 sticky top-0 z-50 bg-opacity-60 backdrop-blur-lg">
       <div className="flex flex-shrink-0 items-center">
         <h1 className="text-2xl md:text-4xl font-bold bg-gradient-to-tr ml-2 from-orange-950 via-orange-500 to-orange-300 bg-clip-text text-transparent">
           AV
